22fd61bc2b78a98792a2afd92714381d03e28267
misket <miskbay@gmail.com>
2014-05-25 16:41:01 +0300

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions submitted for the assignment essentially works 
+# the same way as the example functions indicated in the assignment.
+# Detailed info is given above both functions.
 
-##this function returns a list of functions which do the following 
-##deeds:
-##----------
+## this function returns a list of functions which do the following 
+## deeds:
+## ----------
 #   - makeMatrix: sets a new matrix even if you did not input it 
-#in the function initially
+# in the function initially
 #   - getMatrix: recalls the matrix
 #   - copyCachedInv: runs with the 'cacheSolve' function to cache 
-#the inverse matrix
+# the inverse matrix
 #   - getCache: returns the cached inverse matrix calculated using
-#'cacheSolve'
+# 'cacheSolve'
 
 makeCacheMatrix <- function(x = matrix()) {
     
@@ -24,14 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
     getCache <- function() inv
     
     funclst <- list(make = make, get = get,
-         copyCachedInv = copyCachedInv,
-         getCache = getCache)
-
+                    copyCachedInv = copyCachedInv,
+                    getCache = getCache)
+    
     return(funclst)
 }
 
-
-## Write a short comment describing this function
+## This function (1) checks whether the inverse of a 
+# matrix was calculated in the 'makeCacheMAtrix' function. If so,
+# it just prompts the cached inverse.
+# If the inverse is not calculated earlier (no cached data present)
+# (2) it simply calculates the inverse matrix, and caches it.
 
 cacheSolve <- function(x, ...) {
     inv <- x$getCache()
